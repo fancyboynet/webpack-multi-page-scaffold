9a0a148bd95ef95f3610d86b33b3a4989e6451ec
Fancy <fancyboynet@gmail.com>
2018-05-07 15:09:55 +0800

    增加autoprefix

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -55,7 +55,16 @@ module.exports = {
         test: /\.css$/,
         use: [
           isDevMode ? "style-loader": MiniCssExtractPlugin.loader,
-          'css-loader'
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('autoprefixer')()
+              ]
+            }
+          }
         ]
       },
       {
