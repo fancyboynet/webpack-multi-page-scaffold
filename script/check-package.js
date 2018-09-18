58d493eea0ae65ff7a94744710f91dac029cf848
Fancy <fancyboynet@gmail.com>
2018-09-18 10:40:40 +0800

    增加检测package.json变化功能

--- script/check-package.js
+++ script/check-package.js
@@ -1,1 +1,18 @@
-console.log(1111111111111)
+const buildConfig = require('../config/build')
+const shell = require('shelljs')
+if (!buildConfig.reInstallOnPkgChange) {
+  shell.exit(1)
+}
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git')
+  shell.exit(1)
+}
+const {stdout} = shell.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD', { silent: true })
+const targetFiles = ['package.json']
+const noTargetChange = targetFiles.every((file) => {
+  return stdout.indexOf(file) === -1
+})
+if (noTargetChange) {
+  shell.exit(1)
+}
+shell.exec('npm i')
