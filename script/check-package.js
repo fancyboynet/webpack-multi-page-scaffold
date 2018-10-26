9f96053f96c69c170a0a5e5378516e516b4a9948
Fancy <fancyboynet@gmail.com>
2018-10-26 09:52:36 +0800

    增加跳过checkout检测的配置

--- script/check-package.js
+++ script/check-package.js
@@ -7,12 +7,19 @@ if (!shell.which('git')) {
   shell.echo('Sorry, this script requires git')
   shell.exit(1)
 }
-const {stdout} = shell.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD', { silent: true })
+const stdout = shell.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD', { silent: true }).stdout
+const currentBranch = shell.exec('git rev-parse --abbrev-ref HEAD', { silent: true }).stdout.replace(/\s$/, '')
+const notReInstallOnPkgChangeFeatures = buildConfig.notReInstallOnPkgChangeFeatures
+if (Array.isArray(notReInstallOnPkgChangeFeatures) && notReInstallOnPkgChangeFeatures.includes(currentBranch)) {
+  shell.echo(`Current branch ${currentBranch} does not need check.`)
+  shell.exit(0)
+}
 const targetFiles = ['package.json']
 const noTargetChange = targetFiles.every((file) => {
   return stdout.indexOf(file) === -1
 })
 if (noTargetChange) {
+  shell.echo('no target files changed.')
   shell.exit(0)
 }
 shell.echo('package.json changed, re-install now.')
