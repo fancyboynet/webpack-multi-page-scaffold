f59c585a3b41aeb8059274e4e2494eb1077f25af
Fancy <fancyboynet@gmail.com>
2018-11-26 10:23:31 +0800

    husky add chalk color support

--- script/check-package.js
+++ script/check-package.js
@@ -1,17 +1,18 @@
 const buildConfig = require('../config/build')
 const shell = require('shelljs')
+const chalk = require('chalk')
 if (!buildConfig.reInstallOnPkgChange) {
   shell.exit(0)
 }
 if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git')
+  console.log(chalk.red('Sorry, this script requires git.'))
   shell.exit(1)
 }
 const stdout = shell.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD', { silent: true }).stdout
 const currentBranch = shell.exec('git rev-parse --abbrev-ref HEAD', { silent: true }).stdout.replace(/\s$/, '')
 const notReInstallOnPkgChangeFeatures = buildConfig.notReInstallOnPkgChangeFeatures
 if (Array.isArray(notReInstallOnPkgChangeFeatures) && notReInstallOnPkgChangeFeatures.includes(currentBranch)) {
-  shell.echo(`Current branch ${currentBranch} does not need check.`)
+  console.log(chalk.blue(`Current branch ${currentBranch} does not need check.`))
   shell.exit(0)
 }
 const targetFiles = ['package.json']
@@ -19,9 +20,9 @@ const noTargetChange = targetFiles.every((file) => {
   return stdout.indexOf(file) === -1
 })
 if (noTargetChange) {
-  shell.echo('no target files changed.')
+  console.log(chalk.blue('no target files changed.'))
   shell.exit(0)
 }
-shell.echo('package.json changed, re-install now.')
+console.log(chalk.blue('package.json changed, re-install now.'))
 shell.exec('npm i -d')
 shell.exit(0)
